605f0a5a2b5802dd9dcea6a9a0b213eb6dc53fc3
Sasha Friedrich <sfriedrich@hmc.edu>
2020-01-29 14:21:05 -0800

    Configure symlinks for hg, qumulorc, ssh config, and ssh public key

--- setup_script.sh
+++ setup_script.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
+ln -s $parent_path/hg/hgignore ~/.hgignore
+ln -s $parent_path/hg/hgrc ~/.hgrc
+
+ln -s $parent_path/qumulo/qumulorc ~/.qumulorc
+
+ln -s $parent_path/ssh/config ~/.ssh/config
+ln -s $parent_path/ssh/id_ecdsa.pub ~/.ssh/id_ecdsa.pub
+
 ln -s $parent_path/vim/vimrc ~/.vimrc
