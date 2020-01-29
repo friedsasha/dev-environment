29cbf89665718e199b3e8bfd693804d9605399ed
Sasha Friedrich <sfriedrich@hmc.edu>
2020-01-29 14:52:17 -0800

    Add bash_aliases and zshrc and configure in setup

--- setup_script.sh
+++ setup_script.sh
@@ -10,3 +10,6 @@ ln -s $parent_path/ssh/config ~/.ssh/config
 ln -s $parent_path/ssh/id_ecdsa.pub ~/.ssh/id_ecdsa.pub
 
 ln -s $parent_path/vim/vimrc ~/.vimrc
+
+ln -s $parent_path/bash/bash_aliases ~/.bash_aliases
+ln -s $parent_path/zsh/zshrc ~/.zshrc
