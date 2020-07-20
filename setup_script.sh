e01e892a0072cc263050068926ce5bb9563fd9ed
Sasha Friedrich <sfriedrich@hmc.edu>
2020-07-20 00:12:42 -0700

    Remove qumulo-specific stuff

--- setup_script.sh
+++ setup_script.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-ln -s $parent_path/hg/hgignore ~/.hgignore
-ln -s $parent_path/hg/hgrc ~/.hgrc
-
-ln -s $parent_path/qumulo/qumulorc ~/.qumulorc
-
 ln -s $parent_path/ssh/config ~/.ssh/config
-ln -s $parent_path/ssh/id_ecdsa.pub ~/.ssh/id_ecdsa.pub
 
 ln -s $parent_path/vim/vimrc ~/.vimrc
 
 ln -s $parent_path/bash/bash_aliases ~/.bash_aliases
 
-ln -s $parent_path/zsh/zshrc ~/.zshrc
 ln -s $parent_path/zsh/zshrc.pre-oh-my-zsh ~/.zshrc.pre-oh-my-zsh
 ln -s $parent_path/ohmyzsh ~/.ohmyzsh
 ln -s $parent_path/oh-my-zsh/custom ~/.ohmyzsh-custom
