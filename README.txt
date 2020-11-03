6aa041fea20c2e4b8a16e859ac386b7f520c823e
Sasha Friedrich <sasha.friedrich@gmail.com>
2020-11-03 13:56:42 -0800

    Consolidate dotfiles and add to README

--- README.txt
+++ README.txt
@@ -10,4 +10,9 @@ Then run setup_script.sh. This will link various dot files to ones in this repo.
 Vim plugins should be installed automagically after vim is opened for the first
 time.
 
-
+Change the shell to zsh:
+`cat /etc/shells` to list available shells.
+Sudo access is required to change shells (even for a particular users)
+```
+sudo chsh -s /bin/zsh sasha
+```
--- setup_script.sh
+++ setup_script.sh
@@ -3,9 +3,10 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 ln -s $parent_path/ssh/config ~/.ssh/config
 
-ln -s $parent_path/vim/vimrc ~/.vimrc
+ln -s $parent_path/dotfiles/vimrc ~/.vimrc
+ln -s $parent_path/dotfiles/zshrc ~/.vimrc
 
-ln -s $parent_path/bash/bash_aliases ~/.bash_aliases
+ln -s $parent_path/dotfiles/bash_aliases ~/.bash_aliases
 
 ln -s $parent_path/zsh/zshrc.pre-oh-my-zsh ~/.zshrc.pre-oh-my-zsh
 ln -s $parent_path/ohmyzsh ~/.ohmyzsh
